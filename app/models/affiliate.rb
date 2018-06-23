bbff98d0adf5276507b72619261fa4736c63f7fd
eggmatters <eggmatters@eggmatters.com>
2018-06-23 09:57:04 -0700

    added affiliates breakge method to calculte cost

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,4 +1,19 @@
 
 class Affiliate < Company
-  
+  def self.cost total_affiliates
+    
+    raise "Invalid arguments" if !total_affiliates.is_a?(Numeric)
+    
+    rv = 0
+    case total_affiliates
+    when 0..500
+      rv = 60.00
+    when 501..1000
+      rv = 50.00
+    else total_affiliates > 1000
+      rv = 40.00
+    end
+    
+    rv
+  end
 end
--- spec/models/affiliate_spec.rb
+++ spec/models/affiliate_spec.rb
@@ -19,6 +19,21 @@ RSpec.describe Affiliate, type: :model do
   it "should be named TestAffiliate" do
     expect(subject.name).to eql(@affiliate_hash[:name]);
   end
-
+  
+  it "should return cost for < 500 widgets" do
+    expect(Affiliate.cost 499).to eql(60.00)
+  end
+  
+  it "should return cost for < 1000 widgets" do
+    expect(Affiliate.cost 999).to eql(50.00)
+  end
+  
+  it "should return cost for > 1001 widgets" do
+    expect(Affiliate.cost 1002).to eql(40.00)
+  end
+  
+  it "should raise error for invalid arguments" do
+    expect{Affiliate.cost 'abc'}.to raise_error("Invalid arguments")
+  end
 end
 
