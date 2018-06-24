6d41074f0849d78a5b96e979c87df3cc0231e2fb
eggmatters <eggmatters@eggmatters.com>
2018-06-24 08:01:35 -0700

    Routed both html & json views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'reports/index'
+  get 'reports/index', to: 'reports#index', defaults: { format: 'json' }
+  root 'reports#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
